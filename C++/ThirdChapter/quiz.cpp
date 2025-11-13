02a7c8bf6e65ad792d967ba5720f767279dd5051
david655ai <3568625170@qq.com>
2025-11-13 15:02:42 +0800

    partial practice

---
+++ C++/ThirdChapter/quiz.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+using namespace std;
+// struct personal_info{
+//     string last_name;
+//     string first_name;
+//     char letter;
+//     int age;
+// };quiz1
+
+struct CandyBar
+{
+    string label;
+    double weight;
+    int caro;
+    /* data */
+};//quiz5
+
+int main()
+{
+    // personal_info person1;
+    // cin>>person1.first_name>>person1.last_name>>person1.letter>>person1.age;
+    // cout<<"first name: "<<person1.first_name<<" last name: "<<person1.last_name<<" Grade: "<<(char)(person1.letter+1)<<" age "<<person1.age;//quiz1
+
+    // const int SIZE = 20;
+    // string name,dessert;
+    // cout<<"Enter your name: \n";
+    // cin>>name;
+    // cout<<"Enter your favorite dessert: \n";
+    // cin>>dessert;
+    // cout<<"I have some delicious "<<dessert;
+    // cout<<" for you. "<<name<<".\n";//quiz2
+
+    // char first_name[20];
+    // char last_name[20];
+    // cout<<"Enter your first name: ";
+    // cin.get(first_name,20).get();
+    // cout<<"Enter your last name: ";
+    // cin.get(last_name,20).get();
+    // cout<<"Here's the information in a single string: "<<strcat(strcat(last_name,", "),first_name);
+    //quiz3
+
+    // string first_name;
+    // string last_name;
+    // cout<<"Enter your first name: ";
+    // cin>>first_name;
+    // cout<<"Enter your last name: ";
+    // cin>>last_name;
+    // cout<<"Here's the information in a single string: "<<last_name+", "+first_name;
+    //quiz4
+
+    // CandyBar snack = {"Mocha Munch",2.3,350};
+    // cout<<snack.label<<" "<<snack.weight<<" "<<snack.caro;//quiz5
+
+    // CandyBar snack;
+    // cout<<"Input elementary: \n";
+    // cin>>snack.label>>snack.weight>>snack.caro;
+    // cout<<snack.label<<" "<<snack.weight<<" "<<snack.caro;//quiz6
+    return 0;
+
+}
